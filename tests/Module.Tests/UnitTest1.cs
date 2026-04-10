4e1feb0f1964ecdfc86d15291a1edca526f2b024
Абдулла Арзимуротов <aarzimurotov2001@gmail.com>
2026-04-10 09:45:03 +0300

    CI/CD sozlamalari va NUnit testlar qo'shildi

---
+++ tests/Module.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using Module.Core;
+
+namespace Module.Tests;
+
+public class CalculatorTests
+{
+    private Calculator _calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        Assert.That(_calculator.Add(2, 3), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Subtract_ShouldReturnCorrectDifference()
+    {
+        Assert.That(_calculator.Subtract(5, 3), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Multiply_ShouldReturnCorrectProduct()
+    {
+        Assert.That(_calculator.Multiply(3, 5), Is.EqualTo(15));
+    }
+
+    [Test]
+    public void Divide_ShouldReturnCorrectQuotient()
+    {
+        Assert.That(_calculator.Divide(6, 3), Is.EqualTo(2));
+    }
+}
